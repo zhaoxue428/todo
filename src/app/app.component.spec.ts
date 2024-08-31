b89c06320665215f43cb3a6e90ee95bde0f76bc3
zhaoxue <fromzhaoxue@gmail.com>
2024-09-01 02:10:55 +0900

    use ng test, but one failures, title is not unified

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,15 @@ describe('AppComponent', () => {
   it(`should have the 'todo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('todo');
+    expect(app.title).toEqual('todo!!!');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, todo');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, todo!!!'
+    );
   });
 });
