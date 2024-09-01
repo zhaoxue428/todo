abcb61a14fc0275b219b3218cc9929e66a107254
zhaoxue <fromzhaoxue@gmail.com>
2024-09-01 22:23:26 +0900

    ng generate component welcome --skip-import

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css',
+})
+export class WelcomeComponent {}
